e5d9fae154708a46d64fb920d69fff90c1c6704e
RITA <94054548+ritamahato234@users.noreply.github.com>
2022-10-17 15:41:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+
+const path = require('path');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+app.use(fileUpload(
+    {
+        useTempFiles: true,
+        tempFileDir: path.join(__dirname, 'temp'),
+        createParentPath: true,
+        limits: { fileSize: 1024 * 1024 }
+    }
+))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+    res.render('index')
+})
+
+app.post('/single', (req, res, next) => {
+    try {
+
+        const file = req.files.mFile;
+        const fileName = new Date().getTime().toString() + path.extname(file.name);
+        const savepath = path.join(__dirname, 'public', 'uploads', fileName)
+        if (file.truncated) {
+            throw new Error('file size is too big')
+        }
+        if (file.mimetype !== image / jpeg) {
+            throw new Error('only jpeg allowed')
+
+        }
+        file.mv(savepath)
+        res.redirect('/')
+    } catch (err) {
+        res.render('error')
+    }
+})
+
+app.post('/multiple', (req, res, next) => {
+    try {
+        const files = req.files.mFiles;
+        let arrfiles = [];
+
+/*******another method for uploading multiple files 
+ *files.map((file)=>{
+    const pathSave = path.join(__dirname,'public','uploads',file.name)
+return file.mv(pathSave)
+ })
+ * 
+ * 
+*/
+
+        files.forEach((file) => {
+            const savePath = path.join(__dirname, 'public', 'uploads', file.name);
+            arrfiles.push(file.mv(savePath))
+
+        })
+
+        Promise.all(arrfiles)
+        res.redirect('/');
+    } catch (err) {
+        console.log(err)
+        res.render('error')
+    }
+})
+
+app.listen(3400, () => {
+    console.log(`server is running on http://localhost:3400`);
+})
